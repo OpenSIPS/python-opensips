c5a0606548d3f0135c57191077a12de8ac472b6f
Razvan Crainea <razvan@opensips.org>
2024-10-11 12:40:52 +0300

    setup: add correct license

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
+##
+## This file is part of the OpenSIPS Python Package
+## (see https://github.com/OpenSIPS/python-opensips).
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
+
 from setuptools import setup, find_packages
 
 from opensips import version
@@ -15,7 +34,7 @@ setup(
     url="https://github.com/OpenSIPS/python-opensips",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     scripts=['scripts/opensips-mi'],
