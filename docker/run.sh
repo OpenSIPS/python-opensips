a06a8e0edbf719fab7acc596b1d30a3b30d03dd7
Darius Stefan <darius.stefan@opensips.org>
2024-11-18 15:10:05 +0200

    Fix run.sh: use $ to take the value of CMD

--- docker/run.sh
+++ docker/run.sh
@@ -3,12 +3,13 @@
 CMD=$1
 shift
 
-if [[ CMD == *.py ]]; then
+if [[ $CMD == *.py ]]; then
     TOOL=python3
-elif [[ CMD == *.sh ]]; then
+elif [[ $CMD == *.sh ]]; then
     TOOL=bash
 else
     TOOL=opensips-mi
 fi
 
 exec $TOOL $CMD "$@"
+
