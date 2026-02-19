799ccd579c78ae3494d459059ceccc13ce7bf388
Razvan Crainea <razvan@opensips.org>
2026-02-19 11:59:08 +0200

    packaging: restore setuptools for older architectures

---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+#
+# This file is part of the OpenSIPS Python Package
+# (see https://github.com/OpenSIPS/python-opensips).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Setuptools fallback for legacy distro package builds."""
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+
+def get_version() -> str:
+    version = {}
+    version_file = Path(__file__).parent / "opensips" / "version.py"
+    exec(version_file.read_text(encoding="utf-8"), version)
+    return version["__version__"]
+
+
+setup(
+    name="opensips",
+    version=get_version(),
+    packages=find_packages(),
+    install_requires=[],
+    author="Darius Stefan",
+    author_email="darius.stefan@opensips.org",
+    description="OpenSIPS Python Packages",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/OpenSIPS/python-opensips",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "opensips-mi = opensips.mi.__main__:main",
+            "opensips-event = opensips.event.__main__:main",
+        ]
+    },
+    python_requires=">=3.6",
+)
